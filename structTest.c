cd93479cb22c1bed0a4d7e194f309ab6770cf3ac
elcrity <elcrity@naver.com>
2020-08-04 17:51:34 +0900

    HTML DOM TREE

--- structTest.c
+++ structTest.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 int num = 0;
-int i = 0;
-int istag = 0;
+int n = 0;
 
 typedef struct ElementNode{
     char *data;
@@ -11,18 +10,11 @@ typedef struct ElementNode{
     struct Node *childnode[255]; //각 노드를 하위 노드로 만들기
     struct Node *last;
     struct Node *parent;
-}Node;
-
-typedef struct NodeList{
-    char *data;
-    struct Node *first;
-    struct Node *last;
-    struct Node *parent;
     struct Node *nextSib;
     struct Node *prevSib;
-}List;
+}Node;
 
-Node *newEnode(char *text)
+Node *newNode(char *text)
 {
     Node *current = (Node *)malloc(sizeof(Node));
     current->data = text;
@@ -30,20 +22,6 @@ Node *newEnode(char *text)
     return current;
 }
 
-Node *addEnode(char *text, Node *upper)
-{
-    Node *adEptr = (Node *)malloc(sizeof(Node));
-    adEptr->data = text;
-    adEptr->parent = upper;
-    adEptr->first = adEptr->last = NULL;
-    if(upper->first == NULL) {
-        upper->first = adEptr;
-    }
-    else{
-        upper->last = adEptr;
-    }
-    return adEptr;
-}
 
 Node *addNode(char *text, Node *upper, int cNum)
 {
@@ -63,106 +41,90 @@ Node *addNode(char *text, Node *upper, int cNum)
     }
     return adptr;
 }
-
-Node *addSib(char *text, Node *upper, int cNum)
+//형제노드 x, 형제 노드로 이어줌
+//있으면, 하위노드로, 
+Node *addSib(char *text, Node *upper)
 {
-    Node *adptr = (Node *)malloc(sizeof(Node));
-    adptr->data = text;
-    adptr->parent = upper;
-    adptr->first = adptr->last = NULL;
+    Node *sibptr = (Node *)malloc(sizeof(Node));
+    sibptr->data = text;
+    sibptr->first = sibptr->last = sibptr->prevSib = NULL;
+    sibptr->prevSib = upper;
     // printf("%d\n",cNum);
-    if(upper->first == NULL) {
-        upper->first = adptr;
-        upper->childnode[cNum] = adptr;
-    }
-    else{
-        upper->last = adptr;
-        upper->childnode[cNum] = adptr;
-    }
-    return adptr;
+    upper->nextSib = sibptr;
+    return sibptr;
 }
 
 void main(){
     Node *stp = NULL;
+    Node *sstp = NULL;
+    Node *test = NULL;
     Node *node[100];
     
     char *tagname;
     int c_Num = 0;
-    int hNum = 0;
+    int h_Num = 0;
     int count = 0;
+    int t_Num = 0;
     
     char *tag[100] = {"html","head","title","document","/title","/head",
                         "body",
                         "p","text","/p",
                         "h2","ab","/h2",
                         "div","abcd","/div",
-                        "/body","/html","\0"}; //"img","src","pic_trulli.jpg","alt","\"Trulli\"","width","\"500\"","/"
-    // tagname = "html";
-    // if(strcmp(tag[11],"\0")==0){
-    //     printf("ok\n");
-    // }
-
-    while(strcmp(tag[i],"\0")!=0){//마지막에 /html태그 출력 안됨
+                        "img","src","pic_trulli.jpg","alt","\"Trulli\"","width","\"500\"","/img",
+                        "div","aaaa","/div",
+                        "/body","/html","\0"}; 
+    while(strcmp(tag[n],"\0")!=0){//마지막에 /html태그 출력 안됨
         if(stp==NULL){
-            stp = newEnode(tag[i]);
+            stp = newNode(tag[n]);
             node[num] = stp;
-            // printf("%d\n",num);
-            // printf("1\n");
-            // printf("%s\n",node[num]->data);
-            num++; 
+            num++; count++;
         }
         else{
-            if(strncmp(tag[i],"/",1)==0){// /만나면 상위 노드로
-            // printf("%s\n",stp->data);
-                
-                    for(int i = 0; i< count; i++)
+            if(strncmp(tag[n],"/",1)==0){// /만나면 상위 노드로
+                    for(int i = 0; i < count; i++)
                     {
-                        // printf("asdf");
                         stp = stp->parent;
                         if(strcmp(stp->data,"body")==0)
                         {
                             count--;
                         }
                     }
-                
-                count = 0;
                 c_Num++;
-                // printf("%d\n",num);
-                // printf("2\n");
-                // printf("%s\n",stp->data);
+                count = 0;
             }else if(strcmp(stp->data,"html")==0){
-                stp = addNode(tag[i],stp,hNum);
+                stp = addNode(tag[n],stp,h_Num);
                 node[num] = stp;
-                // printf("%d\n",num);
-                // printf("3\n");
-                printf("%s\n",stp->data);
-                num++; hNum++; count++;
+                printf("%d : %s\n",num, node[num]->data);
+                num++; h_Num++; count;
                 c_Num = 0;
-            }else{
-                if(strcmp(tag[i],"document")==0){
-                    stp = addNode(tag[i],stp,c_Num);
-                    node[num] = stp;
-                    // printf("%d\n",c_Num);
-                    // printf("%d\n",num);
-                    // printf("3\n");
-                    printf("%s\n",node[num]->data);
-                    // printf("%d\n",c_Num);
-                    num++; count++;
-                }else{
-                    // printf("%s\n",tag[i]);
-                    // printf("%d\n",c_Num);
-                    stp = addNode(tag[i],stp,c_Num);
-                    node[num] = stp;
-                    // printf("%d\n",c_Num);
-                    
-                    // printf("3\n");
-                    printf("%s\n",node[num]->data);
-                    num++; count++;
+            }else if(strcmp(stp->data,"img")==0){
+                sstp=stp;
+                while(1){
+                    sstp = addSib(tag[n],sstp);
+                    node[num] = sstp;
+                    printf("%s\n",sstp->data);
+
+                    num++;n++;
+                    node[num]=addNode(tag[n],sstp,0);
+                    if(strcmp(tag[n],"/img")==0){
+                        stp = stp->parent;
+                        break;
+                    }
                 }
+            }else{
+                if(count == 1){
+                    stp = addNode(tag[n],stp,0);
+                }else{
+                    stp = addNode(tag[n],stp,c_Num);
+                }
+                node[num] = stp;
+                printf("%d : %s\n",num, node[num]->data);
+                num++; count++;
+                
             }
-            // printf("%s\n",stp->data);
         }
-        i++;
+        n++;
     }
     // int a = 1;
     // stp=newEnode("body");
@@ -173,23 +135,50 @@ void main(){
     // stp=addNode("img",stp,1);
     // node[2] = stp;
     
-    printf("okokok\n");
+    // printf("okokok\n");
+    // stp = node[0]->childnode[0];
+    // printf("%s\n",stp->data);
+    // stp = node[5]->childnode[0];
+    // printf("%s\n",stp->data);
+    stp = node[0];
+    printf("%s\n",stp->data);
     stp = node[0]->childnode[0];
-    printf("%s\n",stp->data);
+    printf(" ㄴ%s\n",stp->data);
+    stp = node[1]->childnode[0];
+    printf("    ㄴ%s\n",stp->data);
+    stp = node[2]->childnode[0];
+    printf("        ㄴ%s\n",stp->data);
+    stp = node[0]->childnode[1];
+    printf(" ㄴ%s\n",stp->data);
     stp = node[4]->childnode[0];
+    printf("    ㄴ%s\n",stp->data);
+    stp = node[5]->childnode[0];
+    printf("        ㄴ%s\n",stp->data);
+    stp = node[4]->childnode[1];
+    printf("    ㄴ%s\n",stp->data);
+    stp = node[7]->childnode[0];
+    printf("        ㄴ%s\n",stp->data);
+    stp = node[4]->childnode[2];
+    printf("    ㄴ%s\n",stp->data);
+    stp = node[9]->childnode[0];
+    printf("        ㄴ%s\n",stp->data);
+    stp = node[4]->childnode[3];
     printf("%s\n",stp->data);
-    // stp = node[0]->first;
-    // printf("ㄴ%s\n",stp->data);
-    // stp = node[1]->first;
-    // printf("  ㄴ--%s\n",stp->data);
-    // stp = node[2]->first;
-    // printf("ㄴ--%s\n",stp->data);
-    // stp = node[0]->last;
-    // printf("ㄴ%s\n",stp->data);
-    // stp = node[4]->first;
-    // printf("  ㄴ--%s\n",stp->data);
-    // stp = node[4]->last;
-    // printf("  ㄴ--%s\n",stp->data);
+    stp = node[11]->nextSib;
+    printf("    ㅏ%s\n",stp->data);
+    stp = node[12]->childnode[0];
+    printf("    ㅣ   ㄴ%s\n",stp->data);
+    stp = node[13]->nextSib;
+    printf("    ㅏ%s\n",stp->data);
+    stp = node[14]->childnode[0];
+    printf("    ㅣ   ㄴ%s\n",stp->data);
+    stp = node[15]->nextSib;
+    printf("     ㄴ%s\n",stp->data);
+    stp = node[16]->childnode[0];
+    printf("         ㄴ%s\n",stp->data);
+    
+
+
 
     // stp = node[0]->childnode[1];
     // printf("%s\n",stp->data);
